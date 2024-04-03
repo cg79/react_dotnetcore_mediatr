d44e8545020517c201ab1bd5c1a96b9f2d8dec98
Claudiu Gombos <claudiu.gombos@thinslices.com>
2024-04-03 09:12:52 +0300

    adding the repository

---
+++ cqrsVerticalSlices/Functionalities/User/Repository/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using cqrsVerticalSlices.Models;
+
+namespace cqrsVerticalSlices.Functionalities.User.Repository
+{
+    public interface IUserRepository
+    {
+        Task<UserEntity?> GetByIdAsync(int id);
+    }
+}
+
---
+++ cqrsVerticalSlices/Functionalities/User/Repository/UserRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using CQRSVerticalSlices.Data;
+using cqrsVerticalSlices.Models;
+using MediatR;
+
+namespace cqrsVerticalSlices.Functionalities.User.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly IDataContext _context;
+
+        public UserRepository(IDataContext context)
+        {
+            _context = context;
+        }
+
+        public Task<UserEntity?> GetByIdAsync(int id)
+        {
+            var user = _context.Users.FirstOrDefault(u => u.Id == id);
+
+            return Task.FromResult<UserEntity?>(user);
+        }
+    }
+}
+
